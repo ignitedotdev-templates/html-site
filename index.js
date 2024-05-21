ea06de345343fa2cd21260b94714b73b6345a648
CharlesChinedum <charleschinedum2@gmail.com>
2024-05-21 13:54:46 +0100

    chore: Add main section with logo and content to index.html

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-window.alert("Ignite");
+window.onload = function () {
+  const currentPath = window.location.href;
+
+  const pathName = document.getElementsByClassName("pathname")[0];
+
+  if (pathName) {
+    pathName.innerHTML = currentPath;
+    window.alert(currentPath);
+  }
+};
